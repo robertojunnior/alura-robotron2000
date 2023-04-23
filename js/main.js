5f70600f3518d4bd67b96916fe0182bad077b714
roberjunnior <roberjunior.dev@gmail.com>
2023-04-23 02:17:46 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert('satangos');
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener("click", dizOi);
+
+function dizOi(){
+    console.log('Olá,');
+    console.log('Seja bem vindo ao Robotrom 2000!');
+}
+
+dizOi();
