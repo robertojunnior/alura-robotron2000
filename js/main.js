a17e62af14cce133482091b3505727761f1b89ec
roberjunnior <roberjunior.dev@gmail.com>
2023-04-24 01:43:53 -0300

    update-js

--- js/main.js
+++ js/main.js
@@ -2,35 +2,20 @@ const somar = document.querySelector('#somar');
 const subtrair = document.querySelector('#subtrair');
 const braco = document.querySelector('#braco');
 
-//abaixo vemos um exemplo de função 'Anônima - arrow function" ou seja sem nome.
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1;
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
 })
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1;
-})
 
 function manipulaDados(operacao) {
-    if (operacao === "subtrair") {
+    if (operacao === "-") {
         braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
     }
+    console.log(operacao)
 }
-
-
-
-
-
-//podemos utilizar ()=> no lugar do function () {}, junto ao click para função anônima.
-// robotron.addEventListener("click", function (){
-//     console.log("Cliquei no Robô!")
-// });
-
-
-//abaixo temos o exemplo de função declarada.
-// function dizOi(nome){
-//     console.log("Olá " + nome);
-//     console.log('Seja bem vindo ao Robotrom 2000!');
-// }
-
-// dizOi("Junior,");
