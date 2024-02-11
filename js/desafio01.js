5a1d149a3c1410faf53e423eed30406b542426dd
roberjunnior <roberjunior.dev@gmail.com>
2024-02-11 01:19:12 -0300

    update - Atualização de reinicio do projeto

--- js/desafio01.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("#texto")
-//adicionar texto em um <p> com JS podemos usar o .innerHTML
-texto.innerHTML = "O inferno é aqui!";
--- js/desafio02.js
+++
@@ -1,5 +0,0 @@
-const enviarNotas = document.querySelector('#calcular');
-
-enviarNotas.addEventListener("click", (evento) => {
-    console.log("Enviado arrombado!");
-});
