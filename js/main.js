7fdd8d9701d36848d5afb02713d3accdc4141dd7
roberjunnior <roberjunior.dev@gmail.com>
2024-02-14 23:52:30 -0300

    update - adicionando data atributes

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
-    manipulaDados(evento.target.textContent, evento.target.parentNode);
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
   });
 });
 
 function manipulaDados(operacao, controle) {
-  const peca = controle.querySelector(".controle-contador");
+  const peca = controle.querySelector("[data-contador]");
 
   if (operacao === "-") {
     peca.value = parseInt(peca.value) - 1;
